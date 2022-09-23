758b14786d44a5d5ec14ceab19f81c2d64fc9fcd
Tim Hogarty <themaskedtim@gmail.com>
2022-09-23 04:07:18 -0700

    Finish 2nd story with a much more efficient and correct algorithm. Still needs more manual testing for correctness though.

--- server_log_reader.go
+++ server_log_reader.go
@@ -6,7 +6,6 @@ import (
 	"fmt"
 	"io"
 	"os"
-	"sort"
 	"strconv"
 	"strings"
 )
@@ -19,57 +18,23 @@ func check(e error, message string) {
 	}
 }
 
-// Helper method for pulling the search value from the query string
-// Returns -1 if it was unable to pull the value correctly
-func getSearchValueFromQueryString(queryString string) int {
+// Helper method for pulling the search value from the query strings
+// Modified the following algorithm from stack overflow:
+// https://stackoverflow.com/questions/26916952/go-retrieve-a-string-from-between-two-characters-or-other-strings
+// Returns an empty string if no value could be determined.
+func getSearchValueFromQueryString(queryString string) string {
 	startIndex := strings.Index(queryString, "[")
 	if startIndex == -1 {
-			return -1
+			return ""
 	}
 	newString := queryString[startIndex+1:]
 	endIndex := strings.Index(newString, "]")
 	if endIndex == -1 {
-			return -1
+			return ""
 	}
 	searchValueString := newString[:endIndex]
 
-	// Convert the searchValue to an integer
-	searchValue, err := strconv.Atoi(searchValueString)
-	// Error handling the string to int conversion, and print a valuable message for the user
-	check(err, "Failed to find the search value in the queryString. Make sure that the value is an integer and try again.")
-
-	return searchValue
-}
-
-
-// Helper method for determining where the workingIndex for our math on the > and < queries is.
-// Takes in the value of the fileSize list, the queryString for determining the search value, and the boolean that determines if we're heading up or down the list
-// to find the amount of values we're looking for.
-func findWorkingIndex(intList []int, searchValue int, wantLess bool) int {
-	// Create return value holder
-	workingIndex := -1
-
-	// Find the working index and check for duplicates if/when it's found. If the workingIndex is -1
-	// at the end then we should have nothing in the list.
-	// Fixing this and the final few lines of the program would solve a lot of the bugs here.
-	for index, value := range intList {
-		if value >= searchValue {
-			workingIndex = index
-			if !wantLess {
-				for {
-					if workingIndex < len(intList) && workingIndex+1 < len(intList) && intList[workingIndex+1] == searchValue {
-						workingIndex += 1
-					} else {
-						break
-					}
-				}
-			} else {
-				break
-			}
-		}
-	}
-
-	return workingIndex
+	return searchValueString
 }
 
 func main () {
@@ -102,12 +67,37 @@ func main () {
 	csvLogFileReader := csv.NewReader(logFileOpened)
 
 	// Initialize line counter for better logging to user
-	lineNumber := 0
+	lineNumber := 1
 
-	// Create data structures for in memory storage for handling the queries
+	// Create data structures for unique users query
 	uniqueUsers := map[string]int{}
-	uploadSizes := []int{}
-	downloadSizes := []int{}
+
+	// Create boolean that will determine whether or not we want the amount of file sizes above or below our search value
+	wantLess := true
+
+	// Create boolean that determines the type of file action we want to find the aggregate for
+	wantUploads := true
+
+	// Create counters for the amounts of uploads/downloads above/below a certain size
+	uploads := 0
+	downloads := 0
+
+	// Get search value from the string. Only works if we are given a correctly formatted metricsQuery.
+	// Needs better error handling.
+	searchValueString := getSearchValueFromQueryString(*metricsQuery)
+
+	// Convert search value to integer if we are doing an uploads/downloads query
+	searchValue, err := strconv.Atoi(searchValueString)
+	// Check for error in value conversion and inform user of the occurrence if there is one
+	check(err, "Error in retrieving query search value for the > and < query. Please verify your input and try again.")
+
+	// Decision tree for setting boolean values before processing
+	if strings.Contains(*metricsQuery, "downloads") {
+		wantUploads = false
+	}
+	if strings.Contains(*metricsQuery, "GreaterThan") {
+		wantLess = false
+	}
 
 	// Give user information about utilities status
 	fmt.Println("File has been opened and processing is beginning...")
@@ -120,14 +110,17 @@ func main () {
 		csvRecord, err := csvLogFileReader.Read()
 
 		// Skip the first line of the file as it just contains the column names
-		// Increment the lineNumber otherwise for logging
-		if lineNumber == 0 {
+		// Increment the lineNumber before skipping to ensure we don't end up in an infinite loop and can continue processing
+		if lineNumber == 1 {
 			lineNumber += 1
 			continue
 		}
+
+		// Infinite loop termination condition
 		if err == io.EOF {
 			break
 		}
+		// Error message for any other error besides io.EOF
 		check(err, fmt.Sprintf("There was an error reading the file during processing at line #%d. File could not be processed. Please check the contents for errors in formatting and try again", lineNumber))
 
 		// Use map as a makeshift set type by only inputting unique keys. We'll use the length of the map to determine the number of users that have accessed the server.
@@ -138,70 +131,49 @@ func main () {
 			uniqueUsers[currentUserRecord] = 1
 		}
 
-		// Brute forcing the > and < queries. Going to have O(N) memory usage, O(NlogN)+O(N) lookup which simplifies to O(NlogN).
-		// The sorting speed is longer than the lookup speed so that's what wins out in the speed determination.
-		// Using a slice to store the values.
-		// Sorting the values from least to greatest. sorting speed is O(NlogN)
-		// The lookup will be O(N) because we're going to find out where in the slice the
-		// separation point is with iteration, and we'll use math on the current index of the lookup and the length of the slice to
-		// find our answers. There will be one slice for uploads and one slice for downloads.
-		// Going to need to include the duplicates as well in the math. Otherwise we may output the wrong return values.
-		// It's going to be that we find an index of the number we're looking for, or the position it would have been placed, and handle
-		// it from there. If it's there and there are duplicates, we find the index on the side of the duplicates that is towards the direction we
-		// are querying for and do the math. If it's not there then we just take the position it should have been and take the index towards
-		// the direction we are querying for. Left for <, right for >.
-		// Would have liked to have a fancier solution here but ran out of time.
+		// Every query will take O(N) lookup time. We will read the file a single time and answer the query
+		// that was passed in at runtime. Providing the result based on what was requested.
+		// We'll just make the counts of the values we're looking for and return the answer at the end.
 
 		// Temp variables for easy reading later on
 		currentFileAction := csvRecord[2]
-		// CSV read the file sizes as strings so converting them to integers
+		// CSV package reads the file sizes as strings so converting them to integers
 		currentFileSize, err := strconv.Atoi(csvRecord[3])
 		// Error handling the string to int conversion and printing the line number
 		check(err, fmt.Sprintf("Failed converting the file size string to int when processing the record on line #%d", lineNumber))
 
-		// Use the right slice depending on the file action.
-		if currentFileAction == "upload" {
-			uploadSizes = append(uploadSizes, currentFileSize)
-		} else if currentFileAction == "download" {
-			downloadSizes = append(downloadSizes, currentFileSize)
+		// Update the aggregates correctly based on the decision booleans
+		if wantUploads && wantLess && currentFileAction == "upload" && currentFileSize < searchValue {
+			uploads += 1
+		} else if wantUploads && !wantLess && currentFileAction == "upload" && currentFileSize > searchValue {
+			uploads += 1
+		} else if !wantUploads && wantLess && currentFileAction == "download" && currentFileSize < searchValue {
+			downloads += 1
+		} else if !wantUploads && !wantLess && currentFileAction == "download" && currentFileSize > searchValue {
+			downloads += 1
 		}
-
-		// Sort the slices. Default is ascending, which is what we were looking for anyway.
-		sort.Ints(uploadSizes)
-		sort.Ints(downloadSizes)
 
 		// Increment lineNumber for logging
 		lineNumber += 1
 
 	}
 
-	// Inform the user that we've read and processed the file.
-	fmt.Printf("Finished reading and processing file. There were %d records total. Starting query lookup...\n", lineNumber)
+	// Inform the user that we've read and processed the file and tell them how many records there were. We subtract two because the
+	// first line of the file is just the column names and we are over incrementing the lineNumber variable by one simply because of its
+	// increment placement in the processing loop.
+	fmt.Printf("Finished reading and processing file. There were %d records total. The result of your query will be provided below...\n\n", lineNumber-2)
 
-	// Create boolean that will determine whether or not we want the amount of file sizes above or below our search value
-	wantLess := true
-
-	// Get search value from the string. Only works if we are given a correctly formatted metricsQuery.
-	// Needs better error handling.
-	searchValue := getSearchValueFromQueryString(*metricsQuery)
-
-	// Handle query string parsing, lookups, and inform the user of the results.
-	// If the query string was improperly formatted then send the error to the user.
-	// Fixing the workingIndex method and the slice handling would really fix a lot of the bugs in the program
+	// Decision tree for informing user of the results of their query
 	if strings.Contains(*metricsQuery, "usersAccessed") {
 		fmt.Printf("The number of unique users that have accessed the system is: %d\n", len(uniqueUsers))
-	} else if strings.Contains(*metricsQuery, "uploadsGreaterThan[") {
-		workingIndex := findWorkingIndex(uploadSizes, searchValue, !wantLess)
-		fmt.Printf("The number of uploads > %dkB is: %d\n", searchValue, len(uploadSizes[workingIndex:]))
-	} else if strings.Contains(*metricsQuery, "uploadsLessThan[") {
-		workingIndex := findWorkingIndex(uploadSizes, searchValue, wantLess)
-		fmt.Printf("The number of uploads < %dkB is: %d\n", searchValue, len(uploadSizes[:workingIndex]))
-	} else if strings.Contains(*metricsQuery, "downloadsGreaterThan[") {
-		workingIndex := findWorkingIndex(downloadSizes, searchValue, !wantLess)
-		fmt.Printf("The number of downloads > %dkB is: %d\n", searchValue, len(downloadSizes[workingIndex:]))
-	} else if strings.Contains(*metricsQuery, "downloadsLessThan[") {
-		workingIndex := findWorkingIndex(downloadSizes, searchValue, wantLess)
-		fmt.Printf("The number of download < %dkB is: %d\n", searchValue, len(uploadSizes[:workingIndex]))
+	} else if wantUploads && wantLess {
+		fmt.Printf("The number of uploads less than %dkB is: %d\n", searchValue, uploads)
+	} else if wantUploads && !wantLess {
+		fmt.Printf("The number of uploads greater than %dkB is: %d\n", searchValue, uploads)
+	} else if !wantUploads && wantLess {
+		fmt.Printf("The number of downloads less than %dkB is: %d\n", searchValue, downloads)
+	} else if !wantUploads && !wantLess {
+		fmt.Printf("The number of downloads greater than %dkB is: %d\n", searchValue, downloads)
 	}
 
 }
